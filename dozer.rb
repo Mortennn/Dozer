274add6dac31e5c564e791e350a5586b7bfed416
Mortennn <34214574+Mortennn@users.noreply.github.com>
2018-10-04 15:47:33 +0200

    Update dozer.rb

--- dozer.rb
+++ dozer.rb
@@ -1,8 +1,8 @@
 cask 'dozer' do
-  version '1.1.0'
-  sha256 'ac29079a49e3d2705e41eb3a62fb5100b25588a7d137d317e3b5951a5a5a204b'
+  version '1.1.1'
+  sha256 '41909d68be68df4027bb64fa998427dd8cf5879cf8a97333d7a8b97c05dc6415'
   
-  url 'https://github.com/Mortennn/Dozer/releases/tag/1.1.0/Dozer.1.1.0.dmg'
+  url 'https://github.com/Mortennn/Dozer/releases/download/1.1.1/Dozer.1.1.1.dmg'
   appcast "https://raw.githubusercontent.com/Mortennn/Dozer/master/appcast.xml"
   name 'Dozer'
   homepage 'https://github.com/Mortennn/Dozer'
