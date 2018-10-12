c4eb9cd846de9cbf77e19ac0c581d705671c8864
Morten Nautrup Nielsen <mortnau@gmail.com>
2018-10-12 03:29:43 +0200

    k

--- Dozer/AppDelegate.swift
+++ Dozer/AppDelegate.swift
@@ -9,37 +9,79 @@
 import Cocoa
 import MASShortcut
 
-let mainStatusItem = NSStatusBar.system.statusItem(withLength: NSStatusItem.variableLength)
-let mainMenuItemLength:CGFloat = 20
-let blockMenuItemLengthLarge:CGFloat = 10000
+let DozerStatusItem1 = DozerStatusItem()
+let DozerStatusItem2 = DozerStatusItem()
+var DozerStatusItems:[DozerStatusItem] = [DozerStatusItem1, DozerStatusItem2]
+
+class DozerStatusItem {
+  
+  let statusItem = NSStatusBar.system.statusItem(withLength: NSStatusItem.variableLength)
+  
+  let shownLength:CGFloat = 20
+  let hiddenLength:CGFloat = 10000
+  
+  init() {
+    statusItem.length = NSStatusItem.variableLength
+    if let mainStatusItemButton = statusItem.button {
+      mainStatusItemButton.target = self
+      mainStatusItemButton.action = #selector(mainMenuItemClicked(sender:))
+      mainStatusItemButton.image = Icons().shown
+      mainStatusItemButton.sendAction(on: [.leftMouseDown, .rightMouseDown])
+    }
+  }
+  
+  func show() {
+    statusItem.length = shownLength
+  }
+  
+  func hidden() {
+    statusItem.length = hiddenLength
+  }
+  
+  @objc func mainMenuItemClicked(sender: AnyObject?) {
+    guard let currentEvent = NSApp.currentEvent else {
+      NSLog("read current event failed")
+      return
+    }
+    if currentEvent.type == NSEvent.EventType.rightMouseDown {
+//      MainStatusItemWindowController.shared.createWindows()
+      statusItem.length = hiddenLength
+    } else if currentEvent.type == NSEvent.EventType.leftMouseDown {
+      let tmpView = NSView(frame: statusItem.button!.frame)
+      statusItem.button!.addSubview(tmpView)
+      let menu = createMenu(in: tmpView)
+      statusItem.button!.menu = menu
+    }
+  }
+  
+  var isShown:Bool {
+    get {
+      return (statusItem.length == shownLength)
+    }
+  }
+  
+}
 
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
   
   func applicationDidFinishLaunching(_ notification: Notification) {
     
-    firstRun()
+    //firstRun()
     
-    // background menu item
-    mainStatusItem.length = mainMenuItemLength
-    if let mainStatusItemButton = mainStatusItem.button {
-      mainStatusItemButton.target = self
-      mainStatusItemButton.action = #selector(mainMenuItemClicked(sender:))
-      mainStatusItemButton.image = Icons().shown
-      mainStatusItemButton.sendAction(on: [.leftMouseDown, .rightMouseDown])
-    }
-
+    
+    // TODO: fix shortcut
     // bind global shortcut
-    MASShortcutBinder.shared()?.bindShortcut(withDefaultsKey: UserDefaultKeys.Shortcuts.ToggleMenuItems, toAction: {
-      if mainStatusItem.isExpanded {
-        mainStatusItem.length = mainMenuItemLength
-        mainStatusItem.button!.image = Icons().shown
-        MainStatusItemWindowController.shared.removeWindows()
-      } else {
-        mainStatusItem.length = blockMenuItemLengthLarge
-        MainStatusItemWindowController.shared.createWindows()
-      }
-    })
+//    MASShortcutBinder.shared()?.bindShortcut(withDefaultsKey: UserDefaultKeys.Shortcuts.ToggleMenuItems, toAction: {
+//      if mainStatusItem.isExpanded {
+//        mainStatusItem.length = mainMenuItemLength
+//        mainStatusItem.button!.image = Icons().shown
+//        MainStatusItemWindowController.shared.removeWindows()
+//      } else {
+//        mainStatusItem.length = blockMenuItemLengthLarge
+//        MainStatusItemWindowController.shared.createWindows()
+//      }
+//    })
     
     // listens for change is interface theme
     DistributedNotificationCenter.default.addObserver(
@@ -54,34 +96,19 @@ class AppDelegate: NSObject, NSApplicationDelegate {
       name: NSApplication.didChangeScreenParametersNotification,
       object: nil)
   }
+  // FIX: screen did change in resolution
     @objc func screenDidChangeInResolution() {
-      showStatusItems()
-    }
-    
-    @objc func mainMenuItemClicked(sender: AnyObject?) {
-        guard let currentEvent = NSApp.currentEvent else {
-            NSLog("read current event failed")
-            return
-        }
-        if currentEvent.type == NSEvent.EventType.rightMouseDown {
-          MainStatusItemWindowController.shared.createWindows()
-          mainStatusItem.length = blockMenuItemLengthLarge
-        } else if currentEvent.type == NSEvent.EventType.leftMouseDown {
-          let tmpView = NSView(frame: mainStatusItem.button!.frame)
-          mainStatusItem.button!.addSubview(tmpView)
-          let menu = createMenu(in: tmpView)
-          mainStatusItem.button!.menu = menu
-          
-        }
+//      showStatusItems()
     }
   
+  // FIX: handle interface mode changed
    @objc func interfaceModeChanged(sender: NSNotification) {
-    if mainStatusItem.isExpanded {
+//    if mainStatusItem.isExpanded {
 //      let _ = MainStatusItemWindowController.shared.windowInstances.map { $0.backgroundView.image = Icons().hidden }
-      showStatusItems() // FIXME: status items get showed, even when they shouldn't
-    } else {
-      mainStatusItem.button!.image = Icons().shown
-    }
+//      showStatusItems() // FIXME: status items get showed, even when they shouldn't
+//    } else {
+//      mainStatusItem.button!.image = Icons().shown
+//    }
   }
   
   @objc func showPreferences() {
@@ -136,91 +163,89 @@ final class PreferencesController {
     if preferencesController != nil {
       preferencesController!.showWindow(nil)
       NSApp.activate(ignoringOtherApps: true)
-      if !mainStatusItem.isExpanded {
-        MainStatusItemWindowController.shared.removeWindows()
-      }
     }
   }
 }
 
-final class MainStatusItemWindowController {
-  
-  private init() {}
-  
-  static let shared = MainStatusItemWindowController()
-  
-  var windowInstances:[MainStatusItemWindow] = []
-  
-  public func createWindows() {
-    guard let statusItemBtnWindow = mainStatusItem.button?.window else {
-      fatalError("get status item button window failed for: \(mainStatusItem)")
-    }
-    
-    // removes all existing window instances
-    removeWindows()
-    let pixelsToRight = statusItemBtnWindow.screen!.frame.width-(statusItemBtnWindow.frame.origin.x-statusItemBtnWindow.screen!.frame.origin.x)
-    for screen in NSScreen.screens {
-      let frame = NSRect(
-        x: screen.frame.width-pixelsToRight,
-        y: screen.frame.height-22,
-        width: mainMenuItemLength,
-        height: 22)
-      let windowInstance = MainStatusItemWindow(frame: frame, screen:screen)
-      windowInstances.append(windowInstance)
-      windowInstance.orderFront(nil)
-    }
-  }
-  
-  public func removeWindows() {
-    let _ = windowInstances.map { $0.orderOut(nil) }
-    windowInstances = []
-  }
-}
+//final class MainStatusItemWindowController {
+//
+//  private init() {}
+//
+//  static let shared = MainStatusItemWindowController()
+//
+//  var windowInstances:[MainStatusItemWindow] = []
+//
+//  public func createWindows() {
+//    guard let statusItemBtnWindow = mainStatusItem.button?.window else {
+//      fatalError("get status item button window failed for: \(mainStatusItem)")
+//    }
+//
+//    // removes all existing window instances
+//    removeWindows()
+//    let pixelsToRight = statusItemBtnWindow.screen!.frame.width-(statusItemBtnWindow.frame.origin.x-statusItemBtnWindow.screen!.frame.origin.x)
+//    for screen in NSScreen.screens {
+//      let frame = NSRect(
+//        x: screen.frame.width-pixelsToRight,
+//        y: screen.frame.height-22,
+//        width: mainMenuItemLength,
+//        height: 22)
+//      let windowInstance = MainStatusItemWindow(frame: frame, screen:screen)
+//      windowInstances.append(windowInstance)
+//      windowInstance.orderFront(nil)
+//    }
+//  }
+//
+//  public func removeWindows() {
+//    let _ = windowInstances.map { $0.orderOut(nil) }
+//    windowInstances = []
+//  }
+//}
+//
+//final class MainStatusItemWindow: NSPanel {
+//
+//  public var backgroundView:NSImageView!
+//
+//  convenience init(frame:NSRect, screen:NSScreen) {
+//    self.init(contentRect:frame, styleMask: [.nonactivatingPanel], backing: .buffered, defer: false, screen: screen)
+//    self.isOpaque = false
+//    self.hasShadow = false
+//    self.titlebarAppearsTransparent = true
+//    self.titleVisibility = .hidden
+//    self.level = NSWindow.Level(rawValue: (NSWindow.Level.statusBar.rawValue))
+//    self.collectionBehavior = [.canJoinAllSpaces, .fullScreenNone]
+//    self.backgroundColor = NSColor(white: 1, alpha: 0)
+//    let backgroundImageView = NSImageView()
+//    backgroundImageView.autoresizingMask = [.height, .width]
+//    backgroundImageView.image = Icons().hidden
+//    backgroundImageView.frame = self.contentView!.frame
+//    backgroundView = backgroundImageView
+//    self.contentView!.addSubview(backgroundImageView)
+//    self.ignoresMouseEvents = false
+//  }
+//
+//  override func mouseDown(with event: NSEvent) {
+//    let menu = createMenu(in: self.contentView!)
+//    self.menu = menu
+//  }
+//
+//  override func rightMouseDown(with event: NSEvent) {
+//    showStatusItems()
+//  }
+//
+//}
 
-final class MainStatusItemWindow: NSPanel {
-  
-  public var backgroundView:NSImageView!
-  
-  convenience init(frame:NSRect, screen:NSScreen) {
-    self.init(contentRect:frame, styleMask: [.nonactivatingPanel], backing: .buffered, defer: false, screen: screen)
-    self.isOpaque = false
-    self.hasShadow = false
-    self.titlebarAppearsTransparent = true
-    self.titleVisibility = .hidden
-    self.level = NSWindow.Level(rawValue: (NSWindow.Level.statusBar.rawValue))
-    self.collectionBehavior = [.canJoinAllSpaces, .fullScreenNone]
-    self.backgroundColor = NSColor(white: 1, alpha: 0)
-    let backgroundImageView = NSImageView()
-    backgroundImageView.autoresizingMask = [.height, .width]
-    backgroundImageView.image = Icons().hidden
-    backgroundImageView.frame = self.contentView!.frame
-    backgroundView = backgroundImageView
-    self.contentView!.addSubview(backgroundImageView)
-    self.ignoresMouseEvents = false
-  }
-  
-  override func mouseDown(with event: NSEvent) {
-    let menu = createMenu(in: self.contentView!)
-    self.menu = menu
-  }
-  
-  override func rightMouseDown(with event: NSEvent) {
-    showStatusItems()
-  }
-  
-}
+// should be handled in the class itself
+//extension NSStatusItem {
+//  var isExpanded:Bool {
+//    return (self.length == blockMenuItemLengthLarge)
+//  }
+//}
 
-extension NSStatusItem {
-  var isExpanded:Bool {
-    return (self.length == blockMenuItemLengthLarge)
-  }
-}
-
-func showStatusItems() {
-  mainStatusItem.length = mainMenuItemLength
-  for window in MainStatusItemWindowController.shared.windowInstances {
-    window.orderOut(nil)
-  }
-  MainStatusItemWindowController.shared.windowInstances = []
-  mainStatusItem.button!.image = Icons().shown
-}
+//func showStatusItems() {
+//  mainStatusItem.length = mainMenuItemLength
+//  for window in MainStatusItemWindowController.shared.windowInstances {
+//    window.orderOut(nil)
+//  }
+//  MainStatusItemWindowController.shared.windowInstances = []
+//  mainStatusItem.button!.image = Icons().shown
+//}
