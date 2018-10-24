9d6ffe75eadc14221849179b1830b93a8f52c766
Morten Nautrup Nielsen <mortnau@gmail.com>
2018-10-24 21:45:19 +0200

    Update dozer.rb

--- dozer.rb
+++ dozer.rb
@@ -2,7 +2,7 @@ cask 'dozer' do
   version '2.0.0'
   sha256 '2aa0cd777cb37e7aaddd35d2e613b7bcfdfb1317fa13089de0914029d2750b85'
 
-  url "https://github.com/Mortennn/Dozer/releases/download/#{version}/Dozer.#{version}.dmg"
+  url "https://github.com/Mortennn/Dozer/releases/download/v#{version}/Dozer.#{version}.dmg"
   appcast 'https://raw.githubusercontent.com/Mortennn/Dozer/master/appcast.xml'
   name 'Dozer'
   homepage 'https://github.com/Mortennn/Dozer'
