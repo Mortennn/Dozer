98fb64eb6b5acc09363afdd925bd588c9fac49a9
Morten Nautrup Nielsen <mortnau@gmail.com>
2018-10-24 18:09:54 +0200

    improved isMouseInStatusBar

--- Dozer/Logic/HandleMouseMoved.swift
+++ Dozer/Logic/HandleMouseMoved.swift
@@ -1,40 +1,15 @@
 import Cocoa
 
-// legacy
-//func isMouseInStatusBar(with mouseLocation:NSPoint) -> Bool {
-//  let mouseLocationY = mouseLocation.y
-//
-//  let statusBarHeight = NSStatusBar.system.thickness
-//  let screenHeight = NSScreen.main!.frame.height
-//  let screenWidth = NSScreen.main!.frame.width
-//
-//  let offsetY = screenHeight - statusBarHeight
-//  let offsetX = screenWidth - statusBarHeight
-//
-//  return (mouseLocationY >= offsetY)
-//}
-
 func isMouseInStatusBar(with mouseLocation:NSPoint) -> Bool {
   
   let statusBarHeight = NSStatusBar.system.thickness
   
   for screen in NSScreen.screens {
-    
-    let screenX = screen.frame.origin.x
-    let screenY = screen.frame.origin.y
-    let screenHeight = screen.frame.height
-    let screenWidth = screen.frame.width
-    
-    // starting screen
-    if screenX == 0 && screenY == 0 {
-      if (mouseLocation.x <= screenWidth), (mouseLocation.y >= screenHeight - statusBarHeight), (mouseLocation.y <= screenHeight) {
-        return true
-      }
-      // everything else
-    } else {
-      if (mouseLocation.y>=screenHeight+screenY-statusBarHeight) && (mouseLocation.y <= screenY + screenHeight) {
-        return true
-      }
+    var frame = screen.frame
+    frame.origin.y = frame.origin.y + frame.height - statusBarHeight - 2
+    frame.size.height = statusBarHeight + 3
+    if frame.contains(mouseLocation) {
+      return true
     }
   }
   
